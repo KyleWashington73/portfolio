c4a32bd29689bbf742fa093f4c27df4ae6bc8566
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-06-18 23:04:49 -0700

    first commit

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php 
+/** 
+ * Template Name: Homepage 
+ * 
+ * @package Portfolio 
+ * 
+ */ 
+?> 
+<?php get_header(); ?>
+
+<!-- This is the start of hero section -->
+<main id="topBtn">
+        <div class="hero-text">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </main>
+    <!-- This is the end of hero section --> 
