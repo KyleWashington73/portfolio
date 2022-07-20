1cee3dd7dc1910de39662b21b0d920045770516d
KyleWashington73 <91575541+KyleWashington73@users.noreply.github.com>
2022-07-20 13:26:27 -0700

    finally updated!

--- about.php
+++ about.php
@@ -1,63 +1,127 @@
-<?php 
+<?php
+
 /** 
  * Template Name: About 
  * 
  * @package  
  * 
- */ 
-?> 
+ */
+?>
 <?php get_header(); ?>
 
-<!-- This is the start of hero section -->
-       <main id="topBtn" class="main-about">
-        <div class="container-1">
-            <!-- <h1>Hello World</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
-            
+<style>
+   
+</style>
+
+<div class="w3-light-grey main-about">
+
+
+
+
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main w3-content" style="max-width:1600px;margin-top:83px">
+  
+  
+
+  
+
+  <!-- About section -->
+  <div class="w3-container w3-dark-grey w3-center w3-text-light-grey w3-padding-32"  id="about">
+    <h4><b>About Me</b></h4>
+    <img src="http://portfolio.local/wp-content/uploads/2022/07/20220205_133445-1-scaled.jpg" alt="Me" class="w3-image w3-padding-32" width="600" height="650">
+    <div class="w3-content w3-justify" style="max-width:600px">
+      <h4>Hello, my name is Kyle...</h4>
+      <p>And I am a real PROBLEM SOLVER!!!<br>. I have the ability to observe a situation, identify and assess the problem, and come up with a practical SOLUTION very quickly. I also know how to develope websites. I also take pride in being a TEAM PLAYER. I am a WordPress Apprentice and I have the incredible honor of working with a great team of some amazingly talented people. 
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <p>mail: example@example.com</p>
+      <p>tel: 5353 35531</p>
+      <hr class="w3-opacity">
+      <h4 class="w3-padding-16">Technical Skills</h4>
+      <p class="w3-wide">Photography</p>
+      <div class="w3-white">
+        <div class="skill-progress" style="width:95%">95%</div>
+      </div>
+      <p class="w3-wide">Web Design</p>
+      <div class="w3-white">
+        <div class="skill-progress" style="width:85%">85%</div>
+      </div>
+      <p class="w3-wide">Photoshop</p>
+      <div class="w3-white">
+        <div class="skill-progress" style="width:80%">80%</div>
+      </div>
+      <p><button class="w3-button w3-light-grey w3-padding-large w3-margin-top w3-margin-bottom w3-hover-black">Download Resume</button></p>
+      <hr class="w3-opacity">
+
+      
+    </div>
+  </div>
+
+  <!-- Contact section -->
+  <div class="w3-container w3-light-grey w3-padding-32 w3-padding-large" id="contact">
+    <div class="w3-content" style="max-width:600px">
+      <h4 class="w3-center"><b>Contact Me</b></h4>
+      <p>Do you want me to photograph you? Fill out the form and fill me in with the details :) I love meeting new people!</p>
+      <form action="/action_page.php" target="_blank">
+        <div class="w3-section">
+          <label>Name</label>
+          <input class="w3-input w3-border" type="text" name="Name" required>
         </div>
-    </main>
-<!-- This is the end of hero section --> 
-
-<!-- This is the start of info section -->
-
-<!-- This is the end of info section -->
-
-<!-- This is the start of banner section -->
-    <section class="bannerSection" >
-        <div class="bannerImg">
-            <div class="bannerCopy">
-              <!--  <h3>Book Today, Travel Tomorrow</h3> -->
-                <div class="bannerBtn">
-                 <!--   <a href="#">BOOK</a>  -->
-                    <br>
-                </div>
-            </div>
+        <div class="w3-section">
+          <label>Email</label>
+          <input class="w3-input w3-border" type="text" name="Email" required>
         </div>
-    </section>
-<!-- This is the end of banner section -->
-
-<!-- This is the start of footer -->
-<footer>
-        <div class="footerBox">
-            <ul>
-                <a href="/index.html"><li>Home</li></a>
-                <a href="/about.html"><li>About</li></a>
-                <a href="/contact.html"><li>Contact</li></a>
-            </ul>
-
+        <div class="w3-section">
+          <label>Message</label>
+          <input class="w3-input w3-border" type="text" name="Message" required>
         </div>
-        <div class="footerBox">
-            <p>&copy; W4B Neptali Montez</p>
-        </div>
-        <div class="footerBox">
-            <div class="socialMedia">
-                <a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>
-                <a href="https://twitter.com/i/flow/login" target="_blank"><i class="fa-brands fa-twitter-square fa-2x"></i></a>
-                <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-square fa-2x"></i></a>
-            </div>
-        </div>
-</footer>
-    <!-- This is the end of footer -->
+        <button type="submit" class="w3-button w3-block w3-black w3-margin-bottom">Send Message</button>
+      </form>
+    </div>
+  </div>
+
+  <!-- Footer -->
+  <footer class="main-footer w3-padding-32 w3-grey">  
+    <div class="main-footer-wrapper">
+      <h1></h1>
+      <h1></h1>
+      <h1></h1>
+      <h1></h1>
+    </div>
+  </footer>
+  
+  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></div>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+</script>
 
 
+</body>
 
--- portfolio.php
+++ portfolio.php
@@ -19,8 +19,8 @@
          <div class="content">
           <!-- image goes here -->
            <div class="details">
-             <div class="name">Andrew Neil</div>
-             <div class="job">Web Designer</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Children of Incarcerated Parents</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -34,8 +34,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-             <div class="name">Jasmine Carter</div>
-             <div class="job">UI Designer</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Donut Shop</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -49,8 +49,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-             <div class="name">Justin Chung</div>
-             <div class="job">Web Devloper</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Replica Challenge</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -66,8 +66,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-             <div class="name">Appolo Reef</div>
-             <div class="job">Web Designer</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Portfolio</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -81,8 +81,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-             <div class="name">Adrina Calvo</div>
-             <div class="job">UI Designer</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Project Assessment</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -96,8 +96,8 @@
          <div class="content">
            <!-- image goes here -->
            <div class="details">
-             <div class="name">Nicole Lewis</div>
-             <div class="job">Web Devloper</div>
+           <img src="http://portfolio.local/wp-content/uploads/2022/06/img_snow_wide-1.jpg" class="card-image"alt="">
+           <a href=""><div class="link-image">Contact Me!</div></a>
            </div>
            <div class="media-icons">
              <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -117,6 +117,24 @@
      </main>   
 <!-- New Slider -->
 
+<!-- New Footer -->
+<section class="bubble">
+  <!--<h1>Nice Curves</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, asperiores facere iure tempora hic quos sint aperiam totam ipsa, vitae repellendus magnam inventore veniam ut repudiandae eius libero accusantium voluptatem.</p> -->
+  <div class="wave">
+  <!--  <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+    </svg>  -->
+  </div>
+  <div class="curve"></div>
+    <g transform="translate(407.75599138107725 304.6372772694241)" style="visibility: hidden">
+      <path id="blob2" d="M186.8 -166.4C232.1 -141.4 252 -70.7 242.1 -9.9C232.2 50.9 192.5 101.8 147.2 130.8C101.8 159.8 50.9 166.9 -1.8 168.7C-54.4 170.4 -108.9 166.9 -135.4 137.9C-161.9 108.9 -160.4 54.4 -159.9 0.6C-159.3 -53.3 -159.5 -106.5 -133 -131.5C-106.5 -156.5 -53.3 -153.3 8.7 -162C70.7 -170.7 141.4 -191.4 186.8 -166.4" fill="#BB004B"></path>
+    </g>
+  </svg>
+</section>
+<svg id="visual" viewBox="0 0 1350 600" width="1350" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect x="0" y="0" width="1350" height="600" fill="#FFFFFF"></rect><path d="M0 358L32.2 366.3C64.3 374.7 128.7 391.3 193 390.7C257.3 390 321.7 372 386 363.2C450.3 354.3 514.7 354.7 578.8 365.3C643 376 707 397 771.2 412.8C835.3 428.7 899.7 439.3 964 431.2C1028.3 423 1092.7 396 1157 388.8C1221.3 381.7 1285.7 394.3 1317.8 400.7L1350 407L1350 601L1317.8 601C1285.7 601 1221.3 601 1157 601C1092.7 601 1028.3 601 964 601C899.7 601 835.3 601 771.2 601C707 601 643 601 578.8 601C514.7 601 450.3 601 386 601C321.7 601 257.3 601 193 601C128.7 601 64.3 601 32.2 601L0 601Z" fill="#f36b38"></path><path d="M0 408L32.2 416.5C64.3 425 128.7 442 193 451.8C257.3 461.7 321.7 464.3 386 463.2C450.3 462 514.7 457 578.8 453.3C643 449.7 707 447.3 771.2 451.8C835.3 456.3 899.7 467.7 964 467.7C1028.3 467.7 1092.7 456.3 1157 449.3C1221.3 442.3 1285.7 439.7 1317.8 438.3L1350 437L1350 601L1317.8 601C1285.7 601 1221.3 601 1157 601C1092.7 601 1028.3 601 964 601C899.7 601 835.3 601 771.2 601C707 601 643 601 578.8 601C514.7 601 450.3 601 386 601C321.7 601 257.3 601 193 601C128.7 601 64.3 601 32.2 601L0 601Z" fill="#df5031"></path><path d="M0 531L32.2 519.5C64.3 508 128.7 485 193 478.3C257.3 471.7 321.7 481.3 386 489C450.3 496.7 514.7 502.3 578.8 502.5C643 502.7 707 497.3 771.2 492.5C835.3 487.7 899.7 483.3 964 484.5C1028.3 485.7 1092.7 492.3 1157 489.7C1221.3 487 1285.7 475 1317.8 469L1350 463L1350 601L1317.8 601C1285.7 601 1221.3 601 1157 601C1092.7 601 1028.3 601 964 601C899.7 601 835.3 601 771.2 601C707 601 643 601 578.8 601C514.7 601 450.3 601 386 601C321.7 601 257.3 601 193 601C128.7 601 64.3 601 32.2 601L0 601Z" fill="#ca332b"></path><path d="M0 509L32.2 512.2C64.3 515.3 128.7 521.7 193 525.3C257.3 529 321.7 530 386 532.2C450.3 534.3 514.7 537.7 578.8 535.5C643 533.3 707 525.7 771.2 524.7C835.3 523.7 899.7 529.3 964 534.7C1028.3 540 1092.7 545 1157 547.3C1221.3 549.7 1285.7 549.3 1317.8 549.2L1350 549L1350 601L1317.8 601C1285.7 601 1221.3 601 1157 601C1092.7 601 1028.3 601 964 601C899.7 601 835.3 601 771.2 601C707 601 643 601 578.8 601C514.7 601 450.3 601 386 601C321.7 601 257.3 601 193 601C128.7 601 64.3 601 32.2 601L0 601Z" fill="#b40525"></path></svg>
+
+
 
 
   
@@ -126,6 +144,5 @@
 
     
 
-<?php get_footer(); ?> 
 
     
